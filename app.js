9864fc07ab98fcc67ac293794c136b4c8debe4ed
Ji Wang <wj42421999@gmail.com>
2022-08-21 19:06:40 -0400

    Add CORS to allow requests from all origins

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const cors = require('cors')
 
 const trendsRouter = require('./routes/trends')
 const citiesRouter = require('./routes/cities')
 
-const app = express();
+const app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors({
+    origin: "*"
+}))
 
-app.use('/trends', trendsRouter);
-app.use('/cities', citiesRouter);
+app.use('/trends', trendsRouter)
+app.use('/cities', citiesRouter)
 
-module.exports = app;
+module.exports = app
