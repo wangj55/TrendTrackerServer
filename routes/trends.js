d58330c5c104e80274ee76d3e5f363477fc83040
Ji Wang <wj42421999@gmail.com>
2022-08-23 17:56:57 -0400

    Return 404 not found for non-existing WOEID

--- routes/trends.js
+++ routes/trends.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const {getTrendsByWOEID} = require("../utils/database");
+const express = require('express')
+const router = express.Router()
+const {getTrendsByWOEID} = require("../utils/database")
 
 /* GET /trends */
 router.get('/', async function(req, res, next) {
-    const woeid = parseInt(req.query.woeid);
-    const cityDocument = await getTrendsByWOEID(woeid);
-    res.send(cityDocument);
-});
+    const woeid = parseInt(req.query.woeid)
+    const cityDocument = await getTrendsByWOEID(woeid)
+    if (!cityDocument) {
+        res.status(404).send(`Unable to find city with WOEID=${woeid}`)
+    } else {
+        res.send(cityDocument)
+    }
+})
 
-module.exports = router;
+module.exports = router
